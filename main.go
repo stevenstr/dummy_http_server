1b34dfcd62a65e16140f7f04c54d3d21b2787aba
Stevenstr <stephenzhavrid@gmail.com>
2025-02-03 17:06:56 +0300

    create own ServeHTTP method that write byte data to responseWriter.

--- main.go
+++ main.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+type Handler struct{}
+
+func (h Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello world!")
+	res.Write(data)
+}
+
 func main() {
 	log.Println("dummy service is up!")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	var h Handler
+	if err := http.ListenAndServe(":8080", h); err != nil {
 		log.Fatal(err)
 	}
 }
